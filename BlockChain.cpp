eb520e8dbc8fb70d959497ffb350d24bbee0bf42
Ahmed Waheed <creamio@outlook.com>
2019-01-05 15:11:27 +0500

    corrected print function

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -17,38 +17,57 @@ public:
 		void insert(string data)
 		{
 			Block* NewBlock = new Block;
-			NewBlock->hashKey = "2r3yqj4vrqm32gqjy4q2gjm4yq23f4yj";
-			NewBlock->prev_hashKey = "00000000000000000000000000000000";
-			NewBlock->data = data;
-			NewBlock->BlockNum = 1;
-			NewBlock->next = NULL;
-			GenesisBlock = NewBlock;
+			Block* pntr = GenesisBlock;
+			Block* previous_ptr = GenesisBlock;
+			if (GenesisBlock == NULL) {
+				NewBlock->hashKey = "sha256(data)";
+				NewBlock->prev_hashKey = "00000000000000000000000000000000";
+				NewBlock->data = data;
+				NewBlock->BlockNum = 0;
+				NewBlock->next = NULL;
+				GenesisBlock = NewBlock;
+			}
+			else {
+				int counter = 0;
+				while(pntr != NULL) {
+					previous_ptr = pntr;
+					pntr = pntr->next;
+					counter++;
+				}
+				previous_ptr->next = NewBlock;
+				NewBlock->hashKey = "sha256(data)";
+				NewBlock->prev_hashKey = previous_ptr->hashKey;
+				
+				NewBlock->prev = previous_ptr;
+				NewBlock->data = data;
+				NewBlock->BlockNum = counter;
+				NewBlock->next = NULL;
+			}
 		}
 	void print(){
 		Block* curr = GenesisBlock;
-		if(curr->next == NULL){
 			cout<<"BlockNum:  "<<curr->BlockNum<<endl;
 			cout<<"Data:  "<<curr->data<<endl;
 			cout<<"Hash Key:  "<<curr->hashKey<<endl;
 			cout<<"Previous Hash Key:  "<<curr->prev_hashKey<<endl;
-		}
-		else{
-			while(curr->next != NULL){
+			do{
+				curr = curr->next;
 				cout<<"-------------------------------------"<<endl;
 				cout<<"BlockNum:  "<<curr->BlockNum<<endl<<endl;
 				cout<<"Data:  "<<curr->data<<endl;
 				cout<<"Hash Key:  "<<curr->hashKey<<endl;
 				cout<<"Previous Hash Key:  "<<curr->prev_hashKey<<endl;
-				curr = curr->next;
 				cout<<"-------------------------------------"<<endl;
-			}
-		}
+			}while(curr->next != NULL);
 	}
 	
 };
 int main(){
 	chain ch;
 	ch.insert("Hello world!");
+	ch.insert("H---->");
+	ch.insert("e---->");
+	ch.insert("l---->");
 	ch.print();
 	return 0;
 }
